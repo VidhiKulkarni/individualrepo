bad983f3ce95253f7d2db4f6bf517805b54a119c
gigi <gigikwan180@gmail.com>
2022-03-30 15:00:46 -0700

    find primes

--- week2/findprimes.py
+++ week2/findprimes.py
@@ -11,43 +11,12 @@ def findprimes(min, max):
 print()
 
 def primes():
-    minimum = 1
-    print("Imperative- The Minimum Value for Primes Test is 1.")
-    maximum = 20
-    print("Imperative- The Maximum Value for Primes Test is 20.")
-    print("Imperative- The Prime Numbers between ", minimum, "and ", maximum, "is: ")
+    minimum = int(input("Enter the minimum value: "))
+    maximum = int(input("Enter the maximum value: "))
     findprimes(minimum, maximum)
-print()
 
-
-# Define a class for Checking prime number
-class Prime:
-    # Constructor
-    def __init__(self,number) :
-        self.num = number
-    # define a method for checking number is prime or not
-    def isPrime(self) :
-        for i in range(2, int(num ** (1/2)) + 1) :
-            # if any number is divisible by i
-            # then number is not prime
-            # so return False
-            if num % i == 0 :
-                return False
-        # if number is prime then return True
-        return True
 
 
 if __name__ == "__main__" :
     #Imperative
     primes()
-    #OOP: input number
-    num = 29
-    # make an object of Check class
-    print("OOP- Checking if ", num, " is a prime number...")
-    check_prime = Prime(num)
-    # method calling
-    print(check_prime.isPrime())
-    num = 51
-    print("OOP- Checking if ", num, " is a prime number...")
-    check_prime = Prime(num)
-    print(check_prime.isPrime())
